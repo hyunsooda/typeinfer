492bbf1184cb667e26c14f33fbbfbb04a8351f34
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2022-11-23 00:02:47 +0900

    [node] Refined subtree runner

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,5 @@
 use tree_sitter::{Parser, Tree, TreeCursor};
+use tree_sitter::{Point, Range};
 use tree_sitter_traversal::{traverse, Order};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -43,8 +44,22 @@ pub fn get_next_node<'a>(nodes: &'a Vec<Node<'a>>, target: &Node<'a>) -> Option<
     }
 }
 
-pub fn run_subtree<'a>(node: &Node<'a>, code: &'a str, mut f: impl FnMut(&Node<'a>)) {
-    for child in get_nodes(node.info.walk(), Order::Pre, code) {
-        f(&child)
+pub fn run_subtree<'a>(
+    node: &Node<'a>,
+    code: &'a str,
+    mut f: impl FnMut(&Node<'a>) -> Option<Range>,
+) {
+    let mut run_skip = Range {
+        start_byte: 0,
+        end_byte: 0,
+        start_point: Point { row: 0, column: 0 },
+        end_point: Point { row: 0, column: 0 },
+    };
+    for child in get_nodes(node.info.walk(), Order::Pre, code).iter().skip(1) {
+        if run_skip < child.info.range() {
+            if let Some(skip) = f(&child) {
+                run_skip = skip;
+            }
+        }
     }
 }
