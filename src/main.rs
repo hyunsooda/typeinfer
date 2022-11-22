9a8cf03ea60f27da8ac1df117ec71a261a3a588a
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2022-11-23 00:04:20 +0900

    [main] Refactored

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,22 @@ pub mod util;
 use tree_sitter_traversal::Order;
 
 fn main() {
+    // 1. Debloat origin source code to remove control flows
     let filename = "example/example.js";
-    let code = util::read_file(&filename).unwrap();
-    let tree = node::get_tree(&code);
-    let nodes = node::get_nodes(tree.walk(), Order::Pre, &code);
-
-    let debloated_code = debloat::debloat_control_flow(&nodes, &code);
     let debloated_filename = "debloated.js";
-    util::jscode2file(debloated_filename, &debloated_code);
+    debloat::debloat(filename, debloated_filename);
+
+    // 2. Run infer
+    let code = util::read_file(&debloated_filename).unwrap();
+    let tree = node::get_tree(&code);
+    let mut nodes = node::get_nodes(tree.walk(), Order::Pre, &code);
+    assert_eq!(nodes[0].kind(), jssyntax::PROGRAM);
+    nodes.remove(0);
+    infer::run_func(&nodes, &code);
+
+    /*
     for node in nodes {
         println!("{:?}", node);
     }
+    */
 }
