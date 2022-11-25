9bea0d1c3cbe5301794d73f4ca3cc4194588d960
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2022-11-26 02:16:57 +0900

    [util] Added file write function

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,17 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Read;
 use std::io::Write;
 use std::process::Command;
 
-pub fn read_file(file_path: &str) -> std::io::Result<String> {
-    let mut file = File::open(file_path)?;
+pub fn read_file(filename: &str) -> std::io::Result<String> {
+    let mut file = File::open(filename)?;
     let mut data = String::new();
     file.read_to_string(&mut data)?;
     Ok(data)
+}
+
+pub fn write_file(filename: &str, content: &str) {
+    fs::write(filename, content).unwrap()
 }
 
 pub fn jscode2file(filename: &str, code: &str) {
